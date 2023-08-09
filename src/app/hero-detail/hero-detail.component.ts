fda128e80e52c7f3cffae0a0e22fe7827c9e6305
Chris Lopez <44450518+loChris@users.noreply.github.com>
2023-08-09 15:58:09 -0400

    adding persistence features

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -28,6 +28,14 @@ export class HeroDetailComponent {
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    if(this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(
+        () => this.goBack()
+      );
+    }
+  }
+
   goBack(): void {
     this.location.back();
   }
